fe1346b3b6761d22f9270ad88f9f5a05e9196d91
Antoine Baché <antoine.bache@epitech.eu>
2016-04-19 11:56:37 +0200

    Added perlin_calc_pixel

--- include/noise.h
+++ include/noise.h
@@ -1,11 +1,11 @@
 /*
-** noise.h for Raytracer2 in /gfx_raytracer2/include
+1;4401;0c** noise.h for Raytracer2 in /gfx_raytracer2/include
 **
 ** Made by Antoine Baché
 ** Login   <bache_a@epitech.net>
 **
 ** Started on  Mon Apr 18 11:54:08 2016 Antoine Baché
-** Last update Tue Apr 19 09:30:43 2016 Antoine Baché
+** Last update Tue Apr 19 11:48:35 2016 Antoine Baché
 */
 
 #ifndef NOISE_H_
@@ -75,6 +75,30 @@ typedef struct	s_gradient4
   int		p;
 }		t_gradient4;
 
+typedef struct	s_perlin_color2
+{
+  float		a;
+  float		b;
+  float		c;
+  float		d;
+}		t_perlin_color2;
+
+typedef struct	s_perlin_color3
+{
+  float		a[2];
+  float		b[2];
+  float		c[2];
+  float		d[2];
+}		t_perlin_color3;
+
+typedef struct	s_perlin_color4
+{
+  float		a[4];
+  float		b[4];
+  float		c[4];
+  float		d[4];
+}		t_perlin_color4;
+
 double		perlin_noise(void *, float, int);
 
 t_vec2		*perlin_2d_table(void);
@@ -83,5 +107,10 @@ t_vec4		*perlin_4d_table(void);
 void		perlin_4d_table_last(t_vec4 *);
 void		perlin_calc_init(void *, void *, void *, ...);
 void		calc_perlin_gradient(void *, void *, unsigned char *, int);
+void		perlin_calc_smooth_values(void *, void *, void *, int);
+void		perlin_calc_smooth(void *, void *, int);
+void		perlin_calc_color(void *, void *, void *, ...);
+double		perlin_calc_pixel_3d(t_perlin_color3 *, t_vec3 *);
+double		perlin_calc_pixel_4d(t_perlin_color4 *, t_vec4 *);
 
 #endif /* !NOISE_H_ */
---
+++ src/noise/perlin_calc_color.c
@@ -0,0 +1,102 @@
+/*
+** perlin_calc_color.c for Raytracer2 in /gfx_raytracer2/src/noise
+**
+** Made by Antoine Baché
+** Login   <bache_a@epitech.net>
+**
+** Started on  Tue Apr 19 10:21:35 2016 Antoine Baché
+** Last update Tue Apr 19 11:31:32 2016 Antoine Baché
+*/
+
+#include <stdarg.h>
+#include "noise.h"
+
+static void	perlin_calc_color_2d(t_perlin_color2 *color, t_vec2 *pos,
+				     t_ivec2 *origin, ...)
+{
+  va_list	args;
+  t_vec2	*table;
+  t_gradient2	*gradient;
+  float		tmp_x;
+  float		tmp_y;
+
+  va_start(args, origin);
+  gradient = va_args(args, t_gradient2 *);
+  table = va_args(args, t_vec2 *);
+  va_end(args);
+  tmp_x = pos->x - origin->x;
+  tmp_y = pos->y - origin->y;
+  color->a = table[gradient->a].x * tmp_x + table[gradient->a].y * tmp_y;
+  tmp_x = pos->x - (origin->x + 1);
+  color->b = table[gradient->b].x * tmp_x + table[gradient->b].y * tmp_y;
+  tmp_x = pos->x - origin->x;
+  tmp_y = pos->y - (origin->y + 1);
+  color->c = table[gradient->c].x * tmp_x + table[gradient->c].y * tmp_y;
+  tmp_x = pos->x - (origin->x + 1);
+  color->d = table[gradient->d].x * tmp_x + table[gradient->d].y * tmp_y;
+}
+
+static void	perlin_calc_color_3d(t_perlin_color3 *c, t_vec3 *pos,
+				     t_ivec3 *origin, ...)
+{
+  va_list	args;
+  t_vec3	*t;
+  t_gradient3	*g;
+  t_vec3	tmp;
+
+  va_start(args, origin);
+  g = va_args(args, t_gradient3 *);
+  t = va_args(args, t_vec3 *);
+  va_end(args);
+  tmp.x = pos->x - origin->x;
+  tmp.y = pos->y - origin->y;
+  tmp.z = pos->z - origin->z;
+  c->a[0] = t[g->a].x * tmp.x-- + t[g->a].y * tmp.y + t[g->a].z * tmp.z;
+  c->b[0] = t[g->b].x * tmp.x + t[g->b].y * tmp.y-- + t[g->b].z * tmp.z;
+  c->d[0] = t[g->d].x * tmp.x + t[g->d].y * tmp.y + t[g->d].z * tmp.z;
+  tmp.x = pos->x - origin->x;
+  c->c[0] = t[g->c].x * tmp.x + t[g->c].y * tmp.y + t[g->c].z * tmp.z--;
+  tmp.y = pos->y - origin->y;
+  c->a[1] = t[g->a].x * tmp.x-- + t[g->a].y * tmp.y + t[g->a].z * tmp.z;
+  c->b[1] = t[g->b].x * tmp.x + t[g->b].y * tmp.y-- + t[g->b].z * tmp.z;
+  c->d[0] = t[g->d].x * tmp.x + t[g->d].y * tmp.y + t[g->d].z * tmp.z;
+  tmp.x = pos->x - origin->x;
+  c->c[0] = t[g->c].x * tmp.x + t[g->c].y * tmp.y + t[g->c].z * tmp.z;
+}
+
+static void	perlin_calc_color_4d(t_perlin_color4 *color, t_vec4 *pos,
+				     t_ivec4 *origin, ...)
+{
+  va_list	args;
+  t_vec4	*table;
+  t_gradient4	*gradient;
+
+  va_start(args, origin);
+  gradient = va_args(args, t_gradient4 *);
+  table = va_args(args, t_vec4 *);
+  va_end(args);
+}
+
+void		perlin_calc_color(void *color, void *pos,
+				  void *origin, ...)
+{
+  va_list	args;
+  void		*table;
+  void		*gradient;
+  int		dim;
+
+  va_start(args, origin);
+  table = va_arg(args, void *);
+  gradient = va_arg(args, void *);
+  dim = va_arg(args, int);
+  va_end(args);
+  if (dim == 2)
+    perlin_calc_color_2d(color, pos, origin,
+			 (t_gradient2 *)gradient, (t_vec2 *)table);
+  else if (dim == 3)
+    perlin_calc_color_3d(color, pos, origin,
+			 (t_gradient3 *)gradient, (t_vec3 *)table);
+  else if (dim == 4)
+    perlin_calc_color_4d(color, pos, origin,
+			 (t_gradient4 *)gradient, (t_vec4 *)table);
+}
