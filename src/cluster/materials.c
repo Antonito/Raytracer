ac9a0d089f571afb826eb1f7ac352dd100c740e3
Antoine Baché <antoine.bache@epitech.eu>
2016-05-07 17:59:48 +0200

    Material->name should be OK from now on

--- src/cluster/materials.c
+++ src/cluster/materials.c
@@ -5,27 +5,39 @@
 ** Login   <bache_a@epitech.net>
 **
 ** Started on  Sat May  7 17:40:12 2016 Antoine Baché
-** Last update Sat May  7 17:42:33 2016 Antoine Baché
+** Last update Sat May  7 17:58:35 2016 Antoine Baché
 */
 
 #include <unistd.h>
 #include "network.h"
 #include "tools/memory.h"
+#include "tools/str.h"
 
 static void	create_material_packet(char *packet, t_material *material)
 {
   char		*tmp;
-  unsigned int	i;
+  int		i;
+  int		j;
+  int		len;
 
   tmp = (char *)material;
   i = 0;
-  while (i < sizeof(t_material))
+  len = my_strlen(material->name) + 1;
+  while (i < len)
     {
-      packet[i] = tmp[i];
+      packet[i] = material->name[i];
       ++i;
     }
+  len += sizeof(t_material) - 8;
+  j = 8;
+  while (i < len)
+    {
+      packet[i] = tmp[j];
+      ++i;
+      ++j;
+    }
 }
-#pragma message("Attention pointeur *name materials.c")
+
 int		send_materials(int fd, t_material *materials, int nb)
 {
   char		*tmp;
