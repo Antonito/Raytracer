2b893048874a6d73c8c8ff1ba5278edd24f16246
Antoine Baché <antoine.bache@epitech.eu>
2016-05-10 10:11:52 +0200

    Started to free data

--- src/free.c
+++ src/free.c
@@ -5,7 +5,7 @@
 ** Login   <bache_a@epitech.net>
 **
 ** Started on  Tue May 10 09:31:47 2016 Antoine Baché
-** Last update Tue May 10 09:33:23 2016 Antoine Baché
+** Last update Tue May 10 10:10:28 2016 Antoine Baché
 */
 
 #include "raytracer.h"
@@ -13,9 +13,21 @@
 #include "tools/memory.h"
 
 #pragma message("Free data here !")
-int	free_raytracer(t_data *data, int ret)
+int		free_raytracer(t_data *data, int ret)
 {
-  (void)data;
+  t_scene	*tmp;
+  t_scene	*prev;
+
+  tmp = data->scene;
+  while (tmp)
+    {
+      my_free(tmp->lights);
+      my_free(tmp->mat);
+      prev = tmp;
+      tmp = tmp->next;
+      my_free(prev);
+    }
+  my_free(data);
   my_sin(0.0, FREE);
   my_cos(0.0, FREE);
   return (ret);
