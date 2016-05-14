f87df58bf0bd5416992ad65ee57a23df9e1e5f13
boulag_l <boulag_l@epitech.eu>
2016-05-14 20:10:21 +0200

    Corrected output's format

--- toto.sh
+++ toto.sh
@@ -5,7 +5,7 @@ do
 	let "j=$k-1"
 	res=$(echo $res | sed -r "s/([^ ]*) \^ $k/\1 × \1 ^ $j/g")
 done
-echo "$res" | sed -r 's/ \^ 1(.)/\1/g' | sed -r 's/(\(| )([0-9]+)([^0-9\.])/\1\2.0\3/g' | sed -r 's/ ([\+-])/\n\1/g'
+echo "$res" | sed -r 's/ \^ 1(.)/\1/g' | sed -r 's/(\(| )([0-9]+)([^0-9\.])/\1\2.0\3/g'
 echo "============"
 res=$(echo "$1" | sed 's/\+/\n\+/g;s/-/\n-/g' | rev | grep "^t" | sort | sed 's/..//' | rev | sed -r 's/^([^+-])/+\1/' | sed ':a;N;$!ba;s/\n//g' | sed 's/\+/ \+ /g' | sed 's/-/ - /g' | sed 's/\// \/ /g' | sed "s/\*/ × /g" | sed "s/\^/ \^ /g" | sed 's/a/ray->pos.x/g' | sed 's/b/ray->pos.y/g' | sed 's/c/ray->pos.z/g' | sed 's/k/ray->dir.x/g' | sed 's/l/ray->dir.y/g' | sed 's/m/ray->dir.z/g')
 res="$(echo $res | sed -r 's/(.).*$/\1/') t ^ 1 × ($(echo $res | sed 's/..//'))"
@@ -14,7 +14,7 @@ do
 	let "j=$k-1"
 	res=$(echo $res | sed -r "s/([^ ]*) \^ $k/\1 × \1 ^ $j/g")
 done
-echo "$res" | sed -r 's/ \^ 1(.)/\1/g' | sed -r 's/(\(| )([0-9]+)([^0-9\.])/\1\2.0\3/g' | sed -r 's/ ([\+-])/\n\1/g'
+echo "$res" | sed -r 's/ \^ 1(.)/\1/g' | sed -r 's/(\(| )([0-9]+)([^0-9\.])/\1\2.0\3/g'
 for i in {2..100}
 do
 	res=$(echo "$1" | sed 's/\+/\n\+/g;s/-/\n-/g' | rev | grep "^$(echo $i | rev)\^t" | sort | sed "s/^$(echo $i | rev)\^t.//" | rev | sed -r 's/^([^+-])/+\1/' | sed ':a;N;$!ba;s/\n//g' | sed 's/\+/ \+ /g' | sed 's/-/ - /g' | sed 's/\// \/ /g' | sed 's/\*/ × /g' | sed 's/\^/ \^ /g' | sed 's/a/ray->pos.x/g' | sed 's/b/ray->pos.y/g' | sed 's/c/ray->pos.z/g' | sed 's/k/ray->dir.x/g' | sed 's/l/ray->dir.y/g' | sed 's/m/ray->dir.z/g')
@@ -30,5 +30,5 @@ do
 		res=$(echo $res | sed -r "s/([^ ]*) \^ $k/\1 × \1 ^ $j/g")
 	done
 	echo "==============="
-	echo "$res" | sed -r 's/ \^ 1(.)/\1/g' | sed -r 's/(\(| )([0-9]+)([^0-9\.])/\1\2.0\3/g' | sed -r 's/ ([\+-])/\n\1/g'
+	echo "$res" | sed -r 's/ \^ 1(.)/\1/g' | sed -r 's/(\(| )([0-9]+)([^0-9\.])/\1\2.0\3/g'
 done
