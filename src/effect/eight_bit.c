5c9085bce9aa9a6bb3bcee5fe2ae8ae546647f31
boulag_l <boulag_l@epitech.eu>
2016-05-19 21:50:29 +0200

    Adding pixelisation for eight bit

--- src/effect/eight_bit.c
+++ src/effect/eight_bit.c
@@ -5,18 +5,43 @@
 ** Login   <bache_a@epitech.net>
 **
 ** Started on  Wed May 18 14:23:04 2016 Antoine Baché
-** Last update Wed May 18 23:37:51 2016 Antoine Baché
+** Last update Thu May 19 21:49:29 2016 Luka Boulagnon
 */
 
 #include "raytracer.h"
 
-#pragma message("Not implemented")
+void		pixellize(t_color *pix, int width, int height, int intensity)
+{
+  int		x;
+  int		y;
+  int		i;
+  int		j;
+  t_color	color;
+
+  x = -intensity;
+  while ((x += intensity) < width)
+    {
+      y = -intensity;
+      while ((y += intensity) < height)
+	{
+	  color = pix[width * y + x];
+	  i = -1;
+	  while (++i < intensity)
+	    {
+	      j = -1;
+	      while (++j < intensity)
+		pix[width * (y + i) + x + j] = color;
+	    }
+	}
+    }
+}
+
 void		eight_bit_effect(t_color *pix, int height, int width,
 				 t_config *conf)
 {
   int		i;
   int		j;
-  int		cur;
+  int		value;
 
   i = 0;
   (void)conf;
@@ -25,9 +50,18 @@ void		eight_bit_effect(t_color *pix, int height, int width,
       j = 0;
       while (j < width)
 	{
-	  cur = i * width + j;
+	  value = pix[i * width + j].argb[0];
+	  value /= (1 << 4);
+	  pix[i * width + j].argb[0] = value * (1 << 4);
+	  value = pix[i * width + j].argb[1];
+	  value /= (1 << 4);
+	  pix[i * width + j].argb[1] = value * (1 << 4);
+	  value = pix[i * width + j].argb[2];
+	  value /= (1 << 4);
+	  pix[i * width + j].argb[2] = value * (1 << 4);
 	  ++j;
 	}
       ++i;
     }
+  pixellize(pix, width, height, height / 32);
 }
