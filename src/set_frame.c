87ce40c86178e768600e620e8757e377986f5f77
Ludovic Petrenko <ludovic.petrenko@epitech.eu>
2016-05-21 23:35:04 +0200

    debug

--- src/set_frame.c
+++ src/set_frame.c
@@ -5,7 +5,7 @@
 ** Login   <ludonope@epitech.net>
 **
 ** Started on  Mon May  2 22:15:55 2016 Ludovic Petrenko
-** Last update Sat May 21 21:54:47 2016 Antoine Baché
+** Last update Sat May 21 23:12:32 2016 Ludovic Petrenko
 */
 
 #include <stdio.h>
@@ -50,18 +50,18 @@ static void		set_pos(t_ivec2 **vec, t_data *data)
   int			k;
 
   i = k = 0;
-  while (i < WIDTH_20)
+  while (i < data->config.cur_width)
     {
       j = 0;
-      while (j < HEIGHT_20)
+      while (j < data->config.cur_height)
 	{
 	  vec[k][0].x = i;
 	  vec[k][0].y = j;
-	  vec[k][1].x = MIN(i + WIDTH_20, data->config.cur_width);
-	  vec[k++][1].y = MIN(i + HEIGHT_20, data->config.cur_height);
-	  j += HEIGHT_20;
+	  vec[k][1].x = MIN(i + 20, data->config.cur_width);
+	  vec[k++][1].y = MIN(i + 20, data->config.cur_height);
+	  j += 20;
 	}
-      i += WIDTH_20;
+      i += 20;
     }
 }
 
@@ -98,20 +98,17 @@ static void		free_frame(t_ivec2 **pos, int size)
 int			set_frame(t_data *data)
 {
   t_ivec2		**pos;
-  int			size;
 
-  size = (data->config.cur_width / WIDTH_20 + 1) *
-    (data->config.cur_height / HEIGHT_20 + 1);
-  if (!(pos = prepare_tab_frame(size)))
+  if (!(pos = prepare_tab_frame(NB_FRAGMENT)))
     return (1);
   set_vectors(data, &data->scene->cam);
   data->scene->cache->clipable.clip_width = data->config.cur_width;
   data->scene->cache->clipable.clip_height = data->config.cur_height;
   set_pos(pos, data);
   if (data->config.minimum_fps)
-    render_multithread(data, pos, size, LIVE);
+    render_multithread(data, pos, NB_FRAGMENT, LIVE);
   else
-    render_multithread(data, pos, size, RENDER);
-  free_frame(pos, size);
+    render_multithread(data, pos, NB_FRAGMENT, RENDER);
+  free_frame(pos, NB_FRAGMENT);
   return (0);
 }
--- src/threadpool/raytracer.c
+++ src/threadpool/raytracer.c
@@ -5,7 +5,7 @@
 ** Login   <bache_a@epitech.net>
 **
 ** Started on  Fri May 20 20:55:25 2016 Antoine Baché
-** Last update Sat May 21 21:57:12 2016 Antoine Baché
+** Last update Sat May 21 23:34:36 2016 Ludovic Petrenko
 */
 
 #include "threadpool_raytracer.h"
@@ -71,7 +71,7 @@ int				render_multithread(t_data *data, t_ivec2 **pos,
   i = (ret = 0) - 1;
   if (!arg && (loop = -1) && !(arg = init_arg(size, data)))
     return (1);
-  if (state == LIVE && pthread_barrier_init(&barrier, NULL, 5))
+  if (state == LIVE && pthread_barrier_init(&barrier, NULL, NB_THREADS + 1))
     return (1);
   threadpool_set_arg((int [2]){++loop, size}, pos, arg,
 		     (state == LIVE) ? &barrier : NULL);
